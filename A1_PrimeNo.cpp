8fed54470554ea76e33cfa1fb0ccfaeb2437dcb9
Nazia Hassan <116333715+Bluesparx@users.noreply.github.com>
2023-01-10 21:39:38 +0530

    Submitting A1 ques 3

---
+++ A1_PrimeNo.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+bool check(int a){
+    for (int i = 2; i <a; i++){
+        if (a%i==0){
+            return false;
+        }
+        else
+        return true;
+    }
+    if (a==1||a==0){
+        return false; 
+    }
+}
+
+int main(){
+    int number;
+    cout<<"Enter number = ";
+    cin>>number;
+    if (check(number)){
+        cout<<"It is a prime number.";
+    }
+    else
+    cout<<"Not a prime.";
+    return 0;
+}
